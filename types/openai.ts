36b961c0d98c2570bd55a0c1254f730e3e575b1d
comeback01 <comeback01@gmail.com>
2024-01-07 16:11:40 +0100

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -8,7 +8,7 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'togethercomputer/llama-2-13b-chat',
+  GPT_3_5 = 'mistralai/Mistral-7B-Instruct-v0.2',
   
 }
 
@@ -19,7 +19,7 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
     name: 'WebiChat',
-    maxLength: 4000,
+    maxLength: 32000,
     tokenLimit: 2000,
   },
   
